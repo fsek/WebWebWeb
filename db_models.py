754e068142d13ca983dc57e5b85efa2be5b415e3
Oskar Watsfeldt <oskarwats@hotmail.com>
2023-11-20 16:39:53 +0100

    book example

--- db_models.py
+++ db_models.py
@@ -1,4 +1,11 @@
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    MappedAsDataclass,
+    mapped_column,
+    Mapped,
+    relationship,
+)
 
 
 class BaseModelDB(DeclarativeBase, MappedAsDataclass):
