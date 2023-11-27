ce97ff07fd65695bee8a9487695615dcc559e1b2
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2023-11-27 16:57:04 +0100

    starter point for many to many

--- db_models.py
+++ db_models.py
@@ -1,8 +1,33 @@
+from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import (
     DeclarativeBase,
     MappedAsDataclass,
+    Mapped,
+    mapped_column,
+    relationship,
 )
 
 
 class BaseModelDB(DeclarativeBase, MappedAsDataclass):
     pass
+
+
+# TODO create association table. Check docs
+
+
+class Parent_DB(BaseModelDB):
+    __tablename__ = "parent_table"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+
+    name: Mapped[str] = mapped_column()
+
+    # TODO relationship
+
+
+class Child_DB(BaseModelDB):
+    __tablename__ = "child_table"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+
+    age: Mapped[int] = mapped_column()
+
+    # TODO relationship
--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from database import get_db, engine
+from sqlalchemy.orm import Session
 from db_models import BaseModelDB
-from database import engine
-
+from schemas import BaseSchema
 
 app = FastAPI()
 
-# Create database tables
 BaseModelDB.metadata.create_all(engine)
-# Note: SQLite database structre cannot be updated. Delete and run again to update
 
 
-@app.get("/")
-def root():
-    return {"Web web web": "Hell yes"}
+class ParentRead(BaseSchema):
+    id: int
+    # parents: list["ChildRead"]
+    # If we try returning a circular reference from a route we get recursion error
+
+
+class ChildRead(BaseSchema):
+    id: int
+    parents: list[ParentRead]
+
+
+class FamilyCreate(BaseSchema):
+    # TODO replace pass and implement
+    pass
+
+
+@app.get("/make-family", response_model=ChildRead)
+def make_family(family: FamilyCreate, db: Session = Depends(get_db)):
+    # TODO create two parents and a child. Also make sure they're linked
+
+    return
+
+
+@app.get("/abandon-child/{child_id}", response_model=None)
+def lose_child(child_id: int, db: Session = Depends(get_db)):
+    # TODO delete a child
+
+    # Do you also need to delete rows in the association table?
+
+    return None
