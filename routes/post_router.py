d7805b6042a22a0381e1f0b6e7e6e75af77ab690
Benjamin Halasz <benjamin_halasz@icloud.com>
2024-01-30 10:48:47 +0000

    Något konstigt med authorize när jag kallar på create_post from fastapi

--- routes/post_router.py
+++ routes/post_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from database import DB_dependency
 from db_models.post_model import Post_DB
-from api_schemas.post_schemas import PostRead
+from api_schemas.post_schemas import PostRead, PostCreate
+from user.permission import Permission
+from services.post_service import *
 
 post_router = APIRouter()
 
@@ -13,6 +15,13 @@ def get_all_posts(db: DB_dependency):
 
 
 # TODO POST
+
+
+@post_router.post("/", dependencies=[Permission.require("manage", "Post")], response_model=PostRead)
+def create_post(data: PostCreate, db: DB_dependency):
+    post = create_new_post(data, db)
+    return post
+
 
 # TODO DELETE
 
