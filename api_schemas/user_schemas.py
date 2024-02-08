a5a30e76709c326b8ecda53281d3ff6b80b12762
Benjamin Halasz <benjamin_halasz@icloud.com>
2024-02-08 17:16:39 +0000

    fixed telephone number for user and auth on telephone number so we have the right format

--- api_schemas/user_schemas.py
+++ api_schemas/user_schemas.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 from pydantic import StringConstraints
 from fastapi_users import schemas as fastapi_users_schemas
-from helpers.constants import MAX_FIRSTNAME_LEN, MAX_LASTNAME_LEN
+from helpers.constants import MAX_FIRSTNAME_LEN, MAX_LASTNAME_LEN, MAX_TELEPHONE_LEN
 from api_schemas.base_schema import BaseSchema
+from pydantic_extra_types.phone_numbers import PhoneNumber
 
 
 class _UserEventRead(BaseSchema):
@@ -21,12 +22,13 @@ class UserRead(fastapi_users_schemas.BaseUser[int], BaseSchema):
     email: str
     posts: list[_UserPostRead]
     events: list[_UserEventRead]
-
+    telephone_number: str
 
 # fastapi-users will take all fields on this model and feed into the user constructor User_DB(...) when /auth/register route is called
 class UserCreate(fastapi_users_schemas.BaseUserCreate, BaseSchema):
     firstname: Annotated[str, StringConstraints(max_length=MAX_FIRSTNAME_LEN)]
     lastname: Annotated[str, StringConstraints(max_length=MAX_LASTNAME_LEN)]
+    telephone_number: PhoneNumber | None = None
     pass
 
 
