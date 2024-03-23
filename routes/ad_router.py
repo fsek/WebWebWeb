eae2449a48b88079909bb6a258de5ebd5a89663a
Benjamin Halasz <benjamin_halasz@icloud.com>
2024-03-23 16:39:11 +0000

    alla är klara, nu ska de felsökas!

--- routes/ad_router.py
+++ routes/ad_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from api_schemas.ad_schema import AdRead
 from database import DB_dependency
 from db_models.ad_model import BookAd_DB
@@ -26,6 +26,40 @@ def create_ad(data: AdCreate, db: DB_dependency):
     return ad
 
 
-@ad_router.get("/{ad_id}", response_model=AdRead)
-def get_ad(ad_id:int, db: DB_dependency):
-    ad = db.query(BookAd_DB)
+@ad_router.get("/{id}", response_model=AdRead)
+def get_ad_by_id(id:int, db: DB_dependency):
+    ad = db.query(BookAd_DB).filter_by(ad_id=id).one_or_none()
+    if ad is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return ad
+
+@ad_router.get("/{username}", response_model=list[AdRead])
+def get_ad_by_user(username:str, db: DB_dependency):
+    ads = db.query(BookAd_DB).filter_by(seller = username).all()
+    if len(ads) == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return ads
+
+@ad_router.get("/{authorname}", response_model = list[AdRead])
+def get_book_ad_by_author(authorname:str, db: DB_dependency):
+    ads = db.query(BookAd_DB).filter_by(author = authorname).all()
+    if len(ads) == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return ads
+
+@ad_router.get("/{stitle}", response_model = list[AdRead])
+def get_book_ad_by_title(stitle:str, db: DB_dependency):
+    ads = db.query(BookAd_DB).filter_by(title = stitle).all()
+    if len(ads) == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return ads
+
+
+@ad_router.delete("/{id}", response_model=AdRead)
+def remove_ad(id:int, db: DB_dependency):
+    ad = db.query(BookAd_DB).filter_by(ad_id=id).one_or_none()
+    if ad is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    db.delete(ad)
+    db.commit()
+    return ad
