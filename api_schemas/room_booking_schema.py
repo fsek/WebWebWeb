8442d65d36e777745e31b9ca98c01a781c283c3a
Aymen Tilfani <aymen.tilfani@gmail.com>
2024-10-03 02:21:35 +0200

    Add neccessary db models for room booking

---
+++ api_schemas/room_booking_schema.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from helpers.types import datetime_utc
+from pydantic import StringConstraints
+from api_schemas.base_schema import BaseSchema
+from helpers.constants import MAX_CAR_DESC
+
+class RoomRead(BaseSchema):
+    booking_id: int
+    user_id: int
+    description: str
+    start_time: datetime_utc
+    end_time: datetime_utc
+    council_id: int
+
+    
+class CarCreate(BaseSchema):
+    description: Annotated[str, StringConstraints(max_length=MAX_CAR_DESC)] | None = None
+    start_time: datetime_utc
+    end_time: datetime_utc
+    council_id: int
+    
+class CarUpdate(BaseSchema):
+    description: Annotated[str, StringConstraints(max_length=MAX_CAR_DESC)] | None = None
+    start_time: datetime_utc | None = None
+    end_time: datetime_utc | None = None
