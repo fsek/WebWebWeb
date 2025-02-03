4589f0b4946ab470e887d977e565bbb7a1b84ee9
Alexander Lind <alexander.lind03@gmail.com>
2025-02-03 17:55:06 +0000

    try to fix old commit with room booking and councils

--- db_models/room_model.py
+++ db_models/room_model.py
@@ -1,27 +1,27 @@
-# from datetime import datetime
+from datetime import datetime
 
-# from db_models.council_model import Council_DB
-# from .base_model import BaseModel_DB
-# from sqlalchemy.orm import mapped_column, Mapped, relationship
-# from typing import Optional, TYPE_CHECKING
-# from sqlalchemy import ForeignKey
+from db_models.council_model import Council_DB
+from .base_model import BaseModel_DB
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from typing import Optional, TYPE_CHECKING
+from sqlalchemy import ForeignKey
 
-# if TYPE_CHECKING:
-#     from .user_model import User_DB
+if TYPE_CHECKING:
+    from .user_model import User_DB
 
-#     # vision - room with a id, description and booking
+    # vision - room with a id, description and booking
 
 
-# class Room_DB(BaseModel_DB):
-#     __tablename__ = "room_table"
+class Room_DB(BaseModel_DB):
+    __tablename__ = "room_table"
 
-#     id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
 
-#     description: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
 
-#     name: Mapped[Optional[str]] = mapped_column()
+    name: Mapped[Optional[str]] = mapped_column()
 
-#     # kan bara bokas av en person i taget. Kan vara fel key
-#     bookings: Mapped[list["RoomBooking_DB"]] = relationship("RoomBooking_DB", back_populates="room")
+    # kan bara bokas av en person i taget. Kan vara fel key
+    bookings: Mapped[list["RoomBooking_DB"]] = relationship("RoomBooking_DB", back_populates="room")
 
-#     ###Fortsätt lägga till
+    ###Fortsätt lägga till
---
+++ routes/room_router.py
@@ -0,0 +1,12 @@
+# from datetime import datetime
+# from fastapi import APIRouter, HTTPException, status
+# from api_schemas.room_booking_schema import RoomCreate, RoomRead, RoomUpdate
+# from api_schemas.room_booking_schema import RoomRead
+# from database import DB_dependency
+# from typing import Annotated
+# from sqlalchemy import or_, and_
+# from db_models.council_model import Council_DB
+# from user.permission import Permission
+# from db_models.user_model import User_DB
+# from db_models.room_booking_model import RoomBooking_DB
+# from helpers.types import datetime
