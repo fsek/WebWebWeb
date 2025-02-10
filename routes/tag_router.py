05a9db394b1ec7183180953d718be0acf96dabb8
pontahontas <ponberg123@gmail.com>
2025-02-10 17:29:29 +0000

    Gjort så man kan redigera tags. Lagt in en route och en schema.

--- routes/tag_router.py
+++ routes/tag_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from api_schemas.song_schemas import SongCreate, SongRead
-from api_schemas.tag_schema import TagCreate, TagRead
+from api_schemas.tag_schema import TagCreate, TagRead, TagEdit
 from database import DB_dependency
 from db_models.song_model import Song_DB
 from db_models.tag_model import Tag_DB
@@ -11,7 +11,7 @@ from sqlalchemy.exc import IntegrityError
 tag_router = APIRouter()
 
 
-@tag_router.post("/", response_model=TagRead)
+@tag_router.post("/", response_model=TagRead, dependencies=[Permission.require("manage", "Tags")])
 def post_tag(db: DB_dependency, data: TagCreate):
     newtag = Tag_DB(name=data.name)
 
@@ -23,6 +23,19 @@ def post_tag(db: DB_dependency, data: TagCreate):
         raise HTTPException(400, detail="Invalid tag name")
 
     return newtag
+
+
+@tag_router.patch("/", response_model=TagRead, dependencies=[Permission.require("manage", "Tags")])
+def edit_tag(db: DB_dependency, data: TagEdit):
+    tag = db.query(Tag_DB).get(data.id)
+
+    if tag is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    tag.name = data.name
+
+    db.commit()
+    return tag
 
 
 @tag_router.get("/", response_model=list[TagRead])
