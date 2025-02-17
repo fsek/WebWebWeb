db906ceb30d1b053747f3532c121047a2f7c0045
Gloid <peter.niklasson@outlook.com>
2025-02-17 18:48:21 +0100

    Glömde denna filen

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 
 from routes import group_mission_router
 
+from .council_router import council_router
 from .user_router import user_router
 from .post_router import post_router
 from .permission_router import permission_router
@@ -69,3 +70,5 @@ main_router.include_router(nollning_router, prefix="/nollning", tags=["nollning"
 main_router.include_router(group_mission_router, prefix="/group_mission", tags=["nollning"])
 
 main_router.include_router(tag_router, prefix="/tags", tags=["tags"])
+
+main_router.include_router(council_router, prefix="/councils", tags=["council"])
