457eaa55aa24b1545b8f7fc7e8c00a90407102a7
Gloid <peter.niklasson@outlook.com>
2025-02-17 17:10:43 +0000

    Created a new commit for issue 105 and 108, additionalyl solved issue 111

--- routes/post_router.py
+++ routes/post_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from database import DB_dependency
 from db_models.council_model import Council_DB
 from db_models.post_model import Post_DB
-from api_schemas.post_schemas import PostRead, PostCreate
+from api_schemas.post_schemas import PostRead, PostCreate, PostUpdate
 from user.permission import Permission
 from fastapi import status, HTTPException
 
@@ -38,4 +38,13 @@ def delete_post(post_id: int, db: DB_dependency):
     return
 
 
-# TODO PATCH
+@post_router.patch("/{post_id}", dependencies=[Permission.require("manage", "Post")], response_model=PostRead)
+def update_post(post_id: int, updated_post: PostUpdate, db: DB_dependency):
+    post = db.query(Post_DB).filter_by(id=post_id).one_or_none()
+    if post is None:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Cannot find post")
+    for var, value in vars(updated_post).items():
+        setattr(post, var, value) if value else None
+    db.commit()
+    db.refresh(post)
+    return post
