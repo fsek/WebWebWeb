3dd2a6918b54f9949eda8d4c70224f66fd4e7b06
DidRedfors <d.redfors@gmail.com>
2025-02-19 18:22:26 +0100

    fixed get route (#140)

--- api_schemas/council_schema.py
+++ api_schemas/council_schema.py
@@ -1,5 +1,14 @@
 from api_schemas.base_schema import BaseSchema
+from api_schemas.event_schemas import EventRead
+from api_schemas.post_schemas import PostRead
 
 
 class CouncilExempel(BaseSchema):
     exemple_value: int
+
+
+class CouncilRead(BaseSchema):
+    id: int
+    name: str
+    posts: list[PostRead]
+    events: list[EventRead]
--- routes/council_router.py
+++ routes/council_router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from api_schemas.council_schema import CouncilExempel
+from fastapi import HTTPException, status
+from api_schemas.council_schema import CouncilExempel, CouncilRead
 from database import DB_dependency
+from fastapi import APIRouter
 
 from db_models.council_model import Council_DB
 
@@ -11,3 +12,11 @@ council_router = APIRouter()
 def exempel_namn(db: DB_dependency):
     db.query(Council_DB)
     return CouncilExempel(exemple_value=1)
+
+
+@council_router.get("/{council_id}", response_model=CouncilRead)
+def get_council(council_id: int, db: DB_dependency):
+    council = db.query(Council_DB).filter_by(id=council_id).one_or_none()
+    if council is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return council
