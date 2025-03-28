c3f518ca6e6df465583c5e9d367dfc1f97bfc0d7
Didrik Redfors <d.redfors@gmail.com>
2025-03-28 14:10:55 +0000

    document_model

--- db_models/document_model.py
+++ db_models/document_model.py
@@ -1,38 +1,40 @@
 from typing import TYPE_CHECKING, Optional
-from helpers.constants import MAX_BOOK_AUTHOR, MAX_BOOK_TITLE
+from helpers.constants import MAX_DOC_AUTHOR, MAX_BOOK_TITLE, MAX_DOC_TITLE
 from .base_model import BaseModel_DB
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import  ForeignKey, String
+from datetime import datetime
+
 
 if TYPE_CHECKING:
     from .user_model import User_DB
 
 
-class BookAd_DB(BaseModel_DB):
+class Document_DB(BaseModel_DB):
     __tablename__ = "bookad_table"
 
-    ad_id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    document_id: Mapped[int] = mapped_column(primary_key=True, init=False)
 
-    title: Mapped[str] = mapped_column(String(MAX_BOOK_TITLE))
-        
-    course: Mapped[Optional[str]] = mapped_column(String(MAX_BOOK_TITLE))
-    
-    author: Mapped[Optional[str]] = mapped_column(String(MAX_BOOK_AUTHOR))
-    
-    #seller: Mapped[User_DB] = relationship(back_populates = "bookads", init = False)
-            
-    price: Mapped[Optional[int]] = mapped_column(default = 0)
+    title: Mapped[str] = mapped_column(String(MAX_DOC_TITLE))
 
-    selling: Mapped[bool] = mapped_column(default=True)
-    
-    condition: Mapped[int] = mapped_column(default=1) ##1 is best and 3 is worst
-    
+    date: Mapped[datetime] = mapped_column()
+
     user_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey("user_table.id"), default=None
     )
 
     user: Mapped[Optional["User_DB"]] = relationship(
-        back_populates="book_ads", init=False
+        back_populates="documents", init=False
     )
+    
+    #author: Mapped[Optional[str]] = mapped_column(String(MAX_BOOK_AUTHOR))
+    
+    #seller: Mapped[User_DB] = relationship(back_populates = "bookads", init = False)
+            
+    #price: Mapped[Optional[int]] = mapped_column(default = 0)
+
+    #selling: Mapped[bool] = mapped_column(default=True)
+    
+    #condition: Mapped[int] = mapped_column(default=1) ##1 is best and 3 is worst
     
     pass
