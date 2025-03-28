0d11467a9c15c0a713c943eb0a325d705816659e
Gloid <peter.niklasson@outlook.com>
2025-03-28 15:15:15 +0100

    added schema for doc

---
+++ api_schemas/document_schema.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from pydantic import StringConstraints
+from api_schemas.base_schema import BaseSchema
+from helpers.constants import MAX_DOC_AUTHOR, MAX_DOC_TITLE
+from datetime import datetime
+
+class DocumentRead(BaseSchema):
+    document_id: int
+    title: str
+    date: datetime
+    user_id: int
+    
+class DocumentCreate(BaseSchema):
+    title: Annotated[str, StringConstraints(max_length=MAX_DOC_TITLE)]
+    date: datetime
+    user_id: int
+    
+class DocumentUpdate(BaseSchema):
+    title: Annotated[str, StringConstraints(max_length=MAX_DOC_TITLE)] | None = None
+    date: datetime | None = None
