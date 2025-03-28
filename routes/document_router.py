6c50c178aa09b2a40f879daaf74b4159ebe60141
pontahontas <ponberg123@gmail.com>
2025-03-28 14:53:26 +0000

    fixed init

--- routes/document_router.py
+++ routes/document_router.py
@@ -6,10 +6,10 @@ from user.permission import Permission
 from sqlalchemy.exc import IntegrityError, StatementError, SQLAlchemyError
 
 
-document_archive_router = APIRouter()
+document_router = APIRouter()
 
 
-@document_archive_router.post(
+@document_router.post(
     "/", dependencies=[Permission.require("manage", "DocumentArchive")], response_model=DocumentOverview
 )
 def upload_document(db: DB_dependency, data: DocumentUpload):
@@ -18,21 +18,21 @@ def upload_document(db: DB_dependency, data: DocumentUpload):
 
 
 # TODO only users should be able to use this
-@document_archive_router.get("/", response_model=list[DocumentOverview])
+@document_router.get("/", response_model=list[DocumentOverview])
 def get_all_documents(db: DB_dependency):
     accesses = db.query(Documents_DB).all()
 
     return accesses
 
 
-@document_archive_router.patch(
+@document_router.patch(
     "/", dependencies=[Permission.require("manage", "DocumentArchive")], response_model=DocumentOverview
 )
 def update_document(db: DB_dependency, data: DocumentUpdate):
     pass
 
 
-@document_archive_router.delete(
+@document_router.delete(
     "/", dependencies=[Permission.require("manage", "DocumentArchive")], status_code=status.HTTP_204_NO_CONTENT
 )
 def delete_user_access(db: DB_dependency, access_id: int):
