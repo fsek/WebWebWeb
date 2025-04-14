d2a90b86acdefad872e44a30fbd68943923479a5
pontahontas <ponberg123@gmail.com>
2025-04-14 15:57:37 +0000

    Changed order a bit

--- db_models/user_door_access_model.py
+++ db_models/user_door_access_model.py
@@ -14,12 +14,12 @@ if TYPE_CHECKING:
 class UserDoorAccess_DB(BaseModel_DB):
     __tablename__ = "user_door_access_table"
 
+    user_access_id: Mapped[int] = mapped_column(primary_key=True, init=False)
+
     user: Mapped["User_DB"] = relationship(back_populates="accesses", init=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
 
     door: Mapped[Literal[DOOR_ACCESSES]] = mapped_column()
-
-    user_access_id: Mapped[int] = mapped_column(primary_key=True, init=False)
 
     starttime: Mapped[datetime] = mapped_column()
     stoptime: Mapped[datetime] = mapped_column()
