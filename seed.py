ccb3e2d6f4eeb68ec396ee8ece38833254c02597
Samuel Trobbiani <90454757+Trobbi@users.noreply.github.com>
2025-04-18 15:38:25 +0200

    added location as a field in events and also added council in EventRead (#184)
    * added location as a field in events and also added council in EventRead
    
    * forgot to add this lol

--- seed.py
+++ seed.py
@@ -142,12 +142,13 @@ def seed_post_users(db: Session, boss: User_DB, user: User_DB, posts: list[Post_
 
 # Wrapper class for Permission_DB that includes the target posts
 class Permission(Permission_DB):
-    def __init__(self, action=None, target=None, posts: list[str]=[]):
+    def __init__(self, action=None, target=None, posts: list[str] = []):
         super().__init__(action=action, target=target)
         self.posts = posts
 
     def degenerate(self) -> super:
         return Permission_DB(action=self.action, target=self.target)
+
 
 def seed_permissions(db: Session, posts: list[Post_DB]):
 
@@ -207,7 +208,7 @@ def seed_permissions(db: Session, posts: list[Post_DB]):
         Permission(action="manage", target="Nollning", posts=["Buggmästare"]),
         Permission(action="view", target="Nollning", posts=["Buggmästare"]),
         Permission(action="manage", target="Council", posts=["Buggmästare"]),
-        Permission(action="view", target="Council", posts=["Buggmästare"])
+        Permission(action="view", target="Council", posts=["Buggmästare"]),
     ]
 
     [[post.permissions.append(perm.degenerate()) for perm in permissions if post.name in perm.posts] for post in posts]
