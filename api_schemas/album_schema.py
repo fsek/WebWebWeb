ac58f60aa60f1f34c1d62d8a495cf809c7e5826e
Samuel Trobbiani <90454757+Trobbi@users.noreply.github.com>
2025-04-18 15:42:18 +0200

    I realized we also need location and date in on the albums (#190)

--- api_schemas/album_schema.py
+++ api_schemas/album_schema.py
@@ -6,10 +6,14 @@ from helpers.types import datetime_utc
 class AlbumCreate(BaseSchema):
     name: str
     year: int
+    location: str
+    date: datetime_utc
 
 
 class AlbumRead(BaseSchema):
     id: int
     name: str
     year: int
+    date: datetime_utc
+    location: str
     imgs: list[ImgInAlbum]
