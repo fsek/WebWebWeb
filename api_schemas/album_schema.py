bb96b74b7a825244a1b13287a445f1fef2fdc4a5
Samuel Trobbiani <90454757+Trobbi@users.noreply.github.com>
2025-05-14 17:45:06 +0200

    added some more fields to albums (#197)
    * added some more fields to albums
    
    * sanitization
    
    * fixed changes, connected photographed to a user and sanitized the album filepath
    
    * fixed small errors and added albums to user schema
    
    * merged and fixed comment

--- api_schemas/album_schema.py
+++ api_schemas/album_schema.py
@@ -1,10 +1,18 @@
+from typing import TYPE_CHECKING
 from api_schemas.base_schema import BaseSchema
 from api_schemas.img_schema import ImgInAlbum
 from helpers.types import datetime_utc
 
 
+if TYPE_CHECKING:
+    from api_schemas.user_schemas import SimpleUserRead
+
+
 class AlbumCreate(BaseSchema):
-    name: str
+    title_sv: str
+    title_en: str
+    desc_sv: str
+    desc_en: str
     year: int
     location: str
     date: datetime_utc
@@ -12,8 +20,17 @@ class AlbumCreate(BaseSchema):
 
 class AlbumRead(BaseSchema):
     id: int
-    name: str
+    title_sv: str
+    title_en: str
+    desc_sv: str
+    desc_en: str
     year: int
     date: datetime_utc
     location: str
+    photographer: "SimpleUserRead | None"
     imgs: list[ImgInAlbum]
+
+
+class AlbumPhotographerAdd(BaseSchema):
+    user_id: int
+    album_id: int
