3ed877a2b636f2287e8b2f487a84bff854c3869d
maffe03 <manfred.malmros@gmail.com>
2025-07-07 11:05:55 +0200

    Refresh tokens transported as cookies (#221)
    * Speed up development with uv
    
    * Added refresh tokens
    
    * Make OpenAPI return correct response
    
    * Only allow refresh with refresh token
    
    * And also make sure that access tokens use the access lifetimes on refresh, whoops :p
    
    * Better name for cookie
    
    * Added some documentation and improved error handling
    
    * Removed commented out junk
    
    * Set cookie security depending on ENVIRONMENT
    
    * More documentation
    
    * Added Redis :D :D :D
    
    * Created a WHOLE NEW auth_backend for refresh tokens
    
    * Added a (mostly vibecoded) logout-all sessions route
    
    * Probably fine to leave as-is, but maybe being explicit is safer?
    
    * Apparently this function throws...
    whoops! :p
    
    * added redis to tests
    
    * make redis fastapi dependency
    
    * redis healthcheck
    
    * Move LOGIN_TIMEOUT to constant
    
    * make logout routes DELETE
    
    * update gitignore
    
    * Improve secret handling
    
    * needs_refresh returns bool and added better motivation for using sets :p
    
    * Reworked some conditional/panic handling
    
    * Fixed dev/testing with new token
    
    * Update user/user_manager.py
    
    Co-authored-by: Georg <50551748+georgelgeback@users.noreply.github.com>
    
    * Update user/token_strategy.py
    
    Co-authored-by: Georg <50551748+georgelgeback@users.noreply.github.com>
    
    * Reduce access token lifetime to 15 min
    
    ---------
    
    Co-authored-by: Georg <50551748+georgelgeback@users.noreply.github.com>

--- user/token_strategy.py
+++ user/token_strategy.py
@@ -1,8 +1,20 @@
-from typing import TypedDict
+import os
+import secrets
+from typing import Optional, TypedDict, Generic
+from fastapi_users_pelicanq import BaseUserManager
 from fastapi_users_pelicanq.authentication import JWTStrategy
 from db_models.user_model import User_DB
+import redis.asyncio
+from fastapi import Depends
+from fastapi_users_pelicanq.authentication import RedisStrategy, Strategy
+from fastapi_users_pelicanq import models
+from database import get_redis
 
-JWT_SECRET = "MEGA SECRET"
+JWT_SECRET = secrets.token_urlsafe(
+    32
+)  # Generate a secure random secret for JWT signing. JWT lifetime is short, so we can use a random secret.
+# Timeout for refresh token, user has to log in again after expiry
+LOGIN_TIMEOUT = 3600 * 24 * 30
 
 
 # class to describe data in access token for our chosen JWT strategy
@@ -35,6 +47,65 @@ class CustomTokenStrategy(JWTStrategy[User_DB, int]):
         return f"{action}:{target}"
 
 
+class RefreshStrategy(Strategy[models.UP, models.ID]):
+    async def needs_refresh(
+        self, token: Optional[str], user_manager: BaseUserManager[models.UP, models.ID]
+    ) -> Optional[bool]: ...  # pragma: no cover
+    async def destroy_all_tokens(self, user: models.UP) -> None: ...  # pragma: no cover
+
+
+class CustomRedisRefreshStrategy(
+    RedisStrategy[models.UP, models.ID], RefreshStrategy[models.UP, models.ID], Generic[models.UP, models.ID]
+):
+    def __init__(
+        self,
+        redis: redis.asyncio.Redis,
+        lifetime_seconds: int | None = None,
+        refresh_before_seconds: int | None = None,
+        *,
+        key_prefix: str = "fastapi_users_token:",
+    ) -> None:
+        super().__init__(redis, lifetime_seconds, key_prefix=key_prefix)
+        self.refresh_before_seconds = refresh_before_seconds
+
+    async def needs_refresh(self, token: Optional[str], user_manager: BaseUserManager[models.UP, models.ID]) -> bool:
+        if token is None or not self.refresh_before_seconds:
+            return False
+        key = f"{self.key_prefix}{token}"
+        expiry = await self.redis.ttl(key)
+        return (expiry < self.refresh_before_seconds) if expiry > 0 else False
+
+    async def destroy_all_tokens(self, user: models.UP) -> None:
+        """
+        Destroy all tokens for the user.
+        This method is called when the user requests to logout from all sessions.
+        """
+        tokens = await self.redis.smembers(f"{self.key_prefix}user:{user.id}")
+        if tokens:
+            await self.redis.delete(*[f"{self.key_prefix}{token}" for token in tokens])
+            await self.redis.delete(f"{self.key_prefix}user:{user.id}")
+
+    async def write_token(self, user: models.UP) -> str:
+        token = secrets.token_urlsafe(32)  # Is already 32 by default but just to be explicit
+        await self.redis.set(f"{self.key_prefix}{token}", str(user.id), ex=self.lifetime_seconds)
+        # Store the token in a set to be able to manage multiple tokens per user
+        # This allows us to easily invalidate all tokens for a user if needed
+        # Quoting HellFelix: "Using a set here has several benefits:
+        #  - Constant time complexity for add/remove
+        #  - We can assure that there are no duplicates
+        #  - Clean and scalable (easily handles thousands of users)"
+        await self.redis.sadd(f"{self.key_prefix}user:{user.id}", token)
+        return token
+
+
 def get_jwt_strategy() -> JWTStrategy[User_DB, int]:
-    strat = CustomTokenStrategy(secret=JWT_SECRET, lifetime_seconds=3600)
+    strat = CustomTokenStrategy(secret=JWT_SECRET, lifetime_seconds=900)
+    return strat
+
+
+def get_refresh_redis_strategy(
+    redis=Depends(get_redis),
+) -> CustomRedisRefreshStrategy[User_DB, int]:
+    # The refresh tokens do not need to contain permissions
+    strat = CustomRedisRefreshStrategy[User_DB, int](redis, lifetime_seconds=LOGIN_TIMEOUT)
     return strat
