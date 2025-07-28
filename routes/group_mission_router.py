4f733aa5eddfcd2dc5d731ff1901ab68651380c6
trobbi <samueltrobbiani02@gmail.com>
2025-07-28 20:40:10 +0000

    added some checks in create mission group

--- routes/group_mission_router.py
+++ routes/group_mission_router.py
@@ -8,6 +8,11 @@ from db_models.nollning_group_model import NollningGroup_DB
 from services.nollning_service import delete_group_m
 from user.permission import Permission
 from sqlalchemy.exc import IntegrityError
+from typing import Annotated
+from fastapi import APIRouter, HTTPException
+from database import DB_dependency
+from db_models.user_model import User_DB
+from user.permission import Permission
 
 
 group_mission_router = APIRouter()
@@ -16,11 +21,22 @@ group_mission_router = APIRouter()
 @group_mission_router.post(
     "/{nollning_group_id}", dependencies=[Permission.require("view", "Nollning")], response_model=GroupMissionRead
 )
-def add_group_mission(db: DB_dependency, data: GroupMissionCreate, nollning_group_id: int):
+def add_group_mission(
+    db: DB_dependency,
+    data: GroupMissionCreate,
+    nollning_group_id: int,
+    me: Annotated[User_DB, Permission.member()],
+    manage_permission: Annotated[bool, Permission.check("manage", "Nollning")],
+):
     nollning_group = db.query(NollningGroup_DB).filter(NollningGroup_DB.id == nollning_group_id).one_or_none()
 
     if not nollning_group:
         raise HTTPException(404, detail="Nollning group not found")
+
+    nollning_group_user_ids = [user.id for user in nollning_group.group.users]
+
+    if (not me.id in nollning_group_user_ids) and (not manage_permission):
+        raise HTTPException(403, detail="You cannot register a mission for a group you are not in")
 
     adventure_mission = (
         db.query(AdventureMission_DB).filter(AdventureMission_DB.id == data.adventure_mission_id).one_or_none()
@@ -38,8 +54,12 @@ def add_group_mission(db: DB_dependency, data: GroupMissionCreate, nollning_grou
         .filter(GroupMission_DB.adventure_mission_id == adventure_mission.id)
         .one_or_none()
     )
+
     if mission:
         raise HTTPException(400, detail="Group has already made an attempt for this mission")
+
+    if (data.points > adventure_mission.max_points) or (data.points < adventure_mission.min_points):
+        raise HTTPException(400, detail="Point input doesn't match adventure mission points")
 
     mission_group = GroupMission_DB(
         points=data.points,
