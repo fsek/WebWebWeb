14d511b1e2ff75afcf7743dde2a20c8557cc1e8a
Georg <50551748+georgelgeback@users.noreply.github.com>
2025-08-18 18:14:58 +0200

    Fixed spelling error (#371)

--- api_schemas/event_schemas.py
+++ api_schemas/event_schemas.py
@@ -37,7 +37,7 @@ class EventRead(BaseSchema):
     signup_count: int
     dot: str
     lottery: bool
-    event_user_confirmed: bool
+    event_users_confirmed: bool
 
 
 # we dont need to be as strict about out data as in data.
