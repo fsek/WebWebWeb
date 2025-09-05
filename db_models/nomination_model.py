fa7ab5d8dbb12ce787e25da59967023f051b69f3
georgelgeback <georgelgeback@gmail.com>
2025-09-05 10:48:11 +0000

    Fix some leftover errors

--- db_models/nomination_model.py
+++ db_models/nomination_model.py
@@ -1,9 +1,7 @@
-from db_models.election_post_model import ElectionPost_DB
 from .base_model import BaseModel_DB
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey
-from sqlalchemy.ext.associationproxy import association_proxy, AssociationProxy
 from helpers.types import datetime_utc
 from helpers.db_util import created_at_column
 
@@ -19,8 +17,8 @@ class Nomination_DB(BaseModel_DB):
 
     sub_election_id: Mapped[int] = mapped_column(ForeignKey("sub_election_table.sub_election_id"))
 
-    sub_election: Mapped["SubElection_DB"] = relationship(back_populates="candidates", init=False)
     sub_election: Mapped["SubElection_DB"] = relationship(back_populates="nominations", init=False)
+
     nominee_name: Mapped[str] = mapped_column()
 
     nominee_email: Mapped[str] = mapped_column()
@@ -31,6 +29,4 @@ class Nomination_DB(BaseModel_DB):
 
     created_at: Mapped[datetime_utc] = created_at_column()
 
-    election_post: AssociationProxy[ElectionPost_DB] = association_proxy(
-        target_collection="nominations", attr="election_post", init=False
-    )
+    election_post: Mapped["ElectionPost_DB"] = relationship(back_populates="nominations", init=False)
--- db_models/sub_election_model.py
+++ db_models/sub_election_model.py
@@ -2,7 +2,6 @@ from helpers.types import datetime_utc
 from db_models.candidate_model import Candidate_DB
 from db_models.candidate_post_model import Candidation_DB
 from db_models.election_post_model import ElectionPost_DB
-from db_models.election_model import Election_DB
 from db_models.post_model import Post_DB
 from helpers.constants import MAX_SUB_ELECTION_TITLE
 from .base_model import BaseModel_DB
@@ -16,6 +15,8 @@ if TYPE_CHECKING:
     from .candidate_model import Candidate_DB
     from db_models.election_post_model import ElectionPost_DB
     from db_models.post_model import Post_DB
+    from db_models.election_model import Election_DB
+    from db_models.nomination_model import Nomination_DB
 
 
 class SubElection_DB(BaseModel_DB):
@@ -30,7 +31,7 @@ class SubElection_DB(BaseModel_DB):
     end_time: Mapped[datetime_utc] = mapped_column()
 
     election_posts: Mapped[list["ElectionPost_DB"]] = relationship(
-        back_populates="election", cascade="all, delete-orphan"
+        back_populates="sub_election", cascade="all, delete-orphan"
     )
 
     election_id: Mapped[int] = mapped_column(ForeignKey("election_table.election_id"))
@@ -38,12 +39,16 @@ class SubElection_DB(BaseModel_DB):
     election: Mapped["Election_DB"] = relationship(back_populates="sub_elections", init=False)
 
     candidates: Mapped[list["Candidate_DB"]] = relationship(
-        back_populates="election", cascade="all, delete-orphan", init=False
+        back_populates="sub_election", cascade="all, delete-orphan", init=False
+    )
+
+    nominations: Mapped[list["Nomination_DB"]] = relationship(
+        back_populates="sub_election", cascade="all, delete-orphan", init=False
     )
 
     # We need this because we sometimes want to not show what candidate made a candidation
     candidations: Mapped[list["Candidation_DB"]] = relationship(
-        back_populates="election", cascade="all, delete-orphan", init=False
+        back_populates="sub_election", cascade="all, delete-orphan", init=False
     )
 
     posts: AssociationProxy[list["Post_DB"]] = association_proxy(
