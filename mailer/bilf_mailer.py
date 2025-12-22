c7505fe5cd62eeb915eb1d1fb319cf95c3b79dc6
maffe03 <manfred.malmros@gmail.com>
2025-12-22 15:12:35 +0000

    new mailer function

--- mailer/bilf_mailer.py
+++ mailer/bilf_mailer.py
@@ -4,12 +4,11 @@ from zoneinfo import ZoneInfo
 
 
 from email.mime.text import MIMEText
-from db_models.user_model import User_DB
 from db_models.car_booking_model import CarBooking_DB
 from mailer.mail_constants import (
     STANDARD_SENDER,
 )
-from mailer.mail_core import send_mail
+from mailer.mail_core import send_mail_to_adress
 
 
 def bilf_mailer(booking: CarBooking_DB) -> None:
@@ -36,8 +35,8 @@ def bilf_mailer(booking: CarBooking_DB) -> None:
     html = html.replace("{{ booking.time }}", time_string)
 
     if booking.council is not None:
-        html = html.replace("{{ booking.council_en }}", booking.council.name_en)
-        html = html.replace("{{ booking.council_sv }}", booking.council.name_sv)
+        html = html.replace("{{ booking.council_en }}", python_html.escape(booking.council.name_en, quote=True))
+        html = html.replace("{{ booking.council_sv }}", python_html.escape(booking.council.name_sv, quote=True))
 
     msg = MIMEText(html, "html", "utf-8")
 
@@ -48,16 +47,4 @@ def bilf_mailer(booking: CarBooking_DB) -> None:
     else:
         msg["Subject"] = "Ny kollegie-bilbokning / New council car booking"
 
-    # This will surely be super bad
-    fake_user = User_DB(
-        first_name="BilF",
-        last_name="BilF igen",
-        telephone_number="hope this isn't validated",
-        email="bil@fsektionen.se",
-        hashed_password="nope",
-        is_active=True,
-        is_superuser=False,
-        is_verified=True,
-    )
-
-    send_mail(fake_user, msg)
+    send_mail_to_adress("bil@fsektionen.se", msg)
--- mailer/mail_core.py
+++ mailer/mail_core.py
@@ -2,6 +2,8 @@ from email.mime.text import MIMEText
 import os
 import smtplib
 
+from pydantic import EmailStr
+
 from mailer.mail_constants import SMTP_SERVER, STANDARD_SENDER
 from db_models.user_model import User_DB
 
@@ -19,5 +21,22 @@ def send_mail(user: User_DB, msg: MIMEText):
             server.starttls()
             server.sendmail(STANDARD_SENDER, user.email, msg.as_string())
             print(f"Email sent successfully to {user.first_name}")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+
+def send_mail_to_adress(adress: EmailStr, msg: MIMEText):
+    env = os.getenv("ENVIRONMENT")
+
+    if env != "production" and env != "stage":
+        print("Email cannot be used on testing")
+        return
+
+    try:
+        # Create an SMTP session
+        with smtplib.SMTP(SMTP_SERVER, smtplib.SMTP_PORT) as server:
+            server.starttls()
+            server.sendmail(STANDARD_SENDER, adress, msg.as_string())
+            print(f"Email sent successfully to adress {adress}")
     except Exception as e:
         print(f"Failed to send email: {e}")
