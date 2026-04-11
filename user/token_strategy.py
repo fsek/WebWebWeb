b53380ba54c402ea74311731a572c2db4234f1bf
georgelgeback <georgelgeback@gmail.com>
2026-04-11 15:43:36 +0000

    Removed reliance on pelicanq fork of fastapi-users

--- user/token_strategy.py
+++ user/token_strategy.py
@@ -1,13 +1,13 @@
 import os
 import secrets
 from typing import Optional, TypedDict, Generic
-from fastapi_users_pelicanq import BaseUserManager
-from fastapi_users_pelicanq.authentication import JWTStrategy
+from fastapi_users import BaseUserManager
+from fastapi_users.authentication import JWTStrategy
 from db_models.user_model import User_DB
 import redis.asyncio
 from fastapi import Depends
-from fastapi_users_pelicanq.authentication import RedisStrategy, Strategy
-from fastapi_users_pelicanq import models
+from fastapi_users.authentication import RedisStrategy, Strategy
+from fastapi_users import models
 from database import get_redis
 
 
@@ -49,29 +49,6 @@ LOGIN_TIMEOUT = 3600 * 24 * 90
 class AccessTokenData(TypedDict):
     sub: str
     aud: list[str]
-    permissions: list[str]  # this is our own field we add for permission system
-
-
-class CustomTokenStrategy(JWTStrategy[User_DB, int]):
-    # on login we add our own permissions data into the JWT token
-    async def get_user_permissions(self, user: User_DB) -> list[str]:
-        # lets add all permissions form the user's post
-        all_perms: list[str] = []
-        for post in user.posts:
-            for perm in post.permissions:
-                all_perms.append(self.encode_permission(perm.action, perm.target))
-        return all_perms
-
-    @classmethod
-    def decode_permission(cls, permission: str) -> tuple[str, str]:
-        decoded = permission.split(":")
-        action = decoded[0]
-        target = decoded[1]
-        return action, target
-
-    @classmethod
-    def encode_permission(cls, action: str, target: str) -> str:
-        return f"{action}:{target}"
 
 
 class RefreshStrategy(Strategy[models.UP, models.ID]):
@@ -128,7 +105,7 @@ class CustomRedisRefreshStrategy(
 def get_jwt_strategy(
     secret: str = Depends(get_jwt_secret),
 ) -> JWTStrategy[User_DB, int]:
-    strat = CustomTokenStrategy(secret=secret, lifetime_seconds=JWT_TOKEN_LIFETIME_SECONDS)
+    strat = JWTStrategy[User_DB, int](secret=secret, lifetime_seconds=JWT_TOKEN_LIFETIME_SECONDS)
     return strat
 
 
